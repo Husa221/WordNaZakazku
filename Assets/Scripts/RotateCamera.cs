bbee6e02c1c2927beb523905eb562e7e0200e89d
Husa221 <dan.pluhar@seznam.cz>
2024-12-07 13:44:40 +0100

    new stuff

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+
+    [SerializeField] float spinningSpeed = 1f;
+    void Update()
+    {
+        transform.Rotate(0,0,spinningSpeed);
+    }
+}
