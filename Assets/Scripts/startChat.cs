495100f655afbf8e7ec64f73fd9b90fa52cf1ffc
Eliška Szopová <Shopingjs@gmail.com>
2024-12-07 19:34:46 +0100

    dialogue v2

--- Assets/Scripts/startChat.cs
+++ Assets/Scripts/startChat.cs
@@ -21,5 +21,7 @@ public class startChat : MonoBehaviour
     public void onClick()
     {
         dialogueRunner.StartDialogue(gameObject.transform.parent.name);
+        GameObject canvas = GameObject.Find("CanvasOur");
+        canvas.SetActive(false);
     }
 }
